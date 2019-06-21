32bfd74377d48ed407ee6830a645e71a51f5654b
Starling Germosen <sgermosen@outlook.com>
2019-06-20 23:30:21 -0400

    Añadí el identity

--- src/Contactes.Web/Startup.cs
+++ src/Contactes.Web/Startup.cs
@@ -51,6 +51,7 @@ namespace Contactes.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication()
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
